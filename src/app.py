83678ed6971e27f57a6d38953dccdd71ab5f59a5
ninilosso@gmail.com <ikuo.nakajima@linecorp.com>
2021-01-26 11:12:09 +0900

    Deleted lines in html

--- src/app.py
+++ src/app.py
@@ -19,10 +19,6 @@ def show_details() :
            "<body>" + \
            "<table>" + \
            "<tr><td> Start Time </td> <td>" +  startTime + "</td> </tr>" \
-           "<tr><td> Hostname </td> <td>" + utils.gethostname() + "</td> </tr>" \
-           "<tr><td> Local Address </td> <td>" + utils.getlocaladdress() + "</td> </tr>" \
-           "<tr><td> Remote Address </td> <td>" + request.remote_addr + "</td> </tr>" \
-           "<tr><td> Server Hit </td> <td>" + str(hit.getServerHitCount()) + "</td> </tr>" \
            "</table>" + \
            "</body>" + \
            "</html>"
